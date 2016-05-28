a2d36edb7361e08e9acfb7f0c821ff9c6149c6ec
sterben93 <rous.archer@gmail.com>
2016-05-28 10:20:58 -0500

    actualizacion

--- js/main/inscripciones.js
+++ js/main/inscripciones.js
@@ -29,9 +29,39 @@ function agreagrInscritos(inscritos) {
             'html': ins.Cursos_idCursos
         });
         var elemTd7 = $("<td/>", {
-            'html': ins.Status
+            'html': status(ins.Status)
         });
+
         var elemTd8 = $("<td/>");
+        if(ins.Status=="0"){
+            var elemSpan = $('<span/>', {
+                'class': 'glyphicon glyphicon-ok'
+            });
+
+            var elemSpan2 = $('<span/>', {
+                'class': 'glyphicon glyphicon-remove'
+            });
+
+            var elemButton = $('<button/>', {
+                'class': 'btn btn-default insc'
+                , 'val': ins.idInscripciones
+                , 'type': 'button'
+                , 'data-toggle': 'modal'
+                , 'data-target': '#modalIns'
+            });
+            var elemButton2 = $('<button/>', {
+                'class': 'btn btn-default eliminar'
+                , 'val': ins.idInscripciones
+                , 'type': 'button'
+                , 'data-toggle': 'modal'
+                , 'data-target': '#modalEliminar'
+            });
+            elemButton.append(elemSpan);
+            elemButton2.append(elemSpan2);
+            elemTd8.append(elemButton);
+            elemTd8.append(elemButton2);
+        }
+
         var elemSpan = $('<span/>', {
             'class': 'glyphicon glyphicon-ok'
         });
@@ -44,8 +74,7 @@ function agreagrInscritos(inscritos) {
             , 'data-target': '#modalIns'
         });
 
-        elemButton.append(elemSpan);
-        elemTd8.append(elemButton);
+
         elemTr.append(elemTd);
         elemTr.append(elemTd2);
         elemTr.append(elemTd3);
@@ -55,6 +84,17 @@ function agreagrInscritos(inscritos) {
         elemTr.append(elemTd7);
         elemTr.append(elemTd8);
         $("#listaIncriptos").append(elemTr);
-
+        cargarComp());
     })
+}
+
+function cargarComp(){
+
+}
+function status(st){
+    if(st=="0"){
+        return "Pendiente";
+    }else{
+        return 'Aceptado'
+    }
 }
